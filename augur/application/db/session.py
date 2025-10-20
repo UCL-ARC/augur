91e6becbf3c267c32fa1df6df205632298c1d6b1
Mahmoud Abdelrazek <razekmh@gmail.com>
2025-10-20 19:31:57 +0100

    filter null at inser_data

--- augur/application/db/session.py
+++ augur/application/db/session.py
@@ -141,14 +141,18 @@ class DatabaseSession(Session):
             # create a dict that the on_conflict_do_update method requires to be able to map updates whenever there is a conflict. See sqlalchemy docs for more explanation and examples: https://docs.sqlalchemy.org/en/14/dialects/postgresql.html#updating-using-the-excluded-insert-values
             setDict = {}
             for key in data[0].keys():
-                    setDict[key] = getattr(stmnt.excluded, key)
-                
+                setDict[key] = getattr(stmnt.excluded, key)
+                no_null_data_dict = {
+                    k: v
+                    for k, v in setDict.items()
+                    if v not in (None, "", "null", "Null")
+                }
+
             stmnt = stmnt.on_conflict_do_update(
-                #This might need to change
+                # This might need to change
                 index_elements=natural_keys,
-                
-                #Columns to be updated
-                set_ = setDict
+                # Columns to be updated
+                set_=no_null_data_dict,
             )
 
         else:
